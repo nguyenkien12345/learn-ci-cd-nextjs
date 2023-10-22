64e5b64cb7a25586d1a334b2012cbebc7d18d84a
Nguyễn Trung Kiên <kiennt@pivot.vn>
2023-10-22 16:54:05 +0700

    Update test

--- app/blog/[slug]/page.test.tsx
+++ app/blog/[slug]/page.test.tsx
@@ -5,12 +5,6 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Page from './page'
 
-// it('App Router: Works with dynamic route segments', () => {
-//   render(<Page params={{ slug: 'Test' }} />)
-//   expect(screen.getByRole('heading')).toHaveTextContent('Slug: Test')
-// })
-
-
 it('Nguyễn Trung Kiên Và Mai Thị Thanh Thúy should be in the document', () => {
   render(<Page params={{ slug: 'Test' }} />)
   expect(screen.getByText("Nguyễn Trung Kiên Và Mai Thị Thanh Thúy")).toBeInTheDocument();
